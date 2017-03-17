871c50d066be24fd6b915e19f6d29fb78b2ecd60
Asabeneh Yetayeh <asabeneh@gmail.com>
2017-03-17 22:26:26 +0200

    inital commit

---
+++ client/main.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {Meteor} from 'meteor/meteor';
+import {Tracker} from 'meteor/tracker';
+import {Players ,calculatorPlayerPositions}from './../imports/api/players';
+import App from './../imports/ui/App';
+
+
+Meteor.startup(()=>{
+Tracker.autorun(()=>{
+let players = Players.find({},{sort:{score:-1,name:1}}).fetch();
+let positionedPlayers = calculatorPlayerPositions(players)
+let title = "Score Keep"
+
+
+ReactDOM.render(
+<App title = {title} players = {positionedPlayers} />,document.getElementById('app'));
+});
+})
---
+++ imports/utils.js
@@ -0,0 +1,8 @@
+console.log('log from imports/utils.js');
+
+export let greetUser = function(){
+return "Hello,";
+};
+export let name = 'Asabeneh Shitahun';
+
+export default "default value";
